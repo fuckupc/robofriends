750a7644993cf8dac1a04a127b557200946ac903
upcliucong <upcliucong@gmail.com>
2019-04-25 18:29:20 +0800

    Deconstructoring

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Card = (props) => {
+    const { name, email, id } = props;
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow shadow-5'>
-            <img src={ `http://robohash.org/${props.id}?200*200` } alt='robot' />
+            <img src={ `http://robohash.org/${id}?200*200` } alt='robot' />
             <div>
-                <h2>{ props.name }</h2>
-                <p>{ props.email }</p>
+                <h2>{ name }</h2>
+                <p>{ email }</p>
             </div>
         </div>
     )
