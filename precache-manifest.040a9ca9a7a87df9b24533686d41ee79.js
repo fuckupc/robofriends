ebe6feac5967a96bb196df2c53ba6198da561d4a
upcliucong <upcliucong@gmail.com>
2019-04-26 00:53:23 +0800

    Updates

---
+++ precache-manifest.040a9ca9a7a87df9b24533686d41ee79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da03b4f74f0ce78bf0e8f93affc0c17",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ed3b08e45430504bf91d",
+    "url": "/robofriends/static/css/main.7e9508e6.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "ed3b08e45430504bf91d",
+    "url": "/robofriends/static/js/main.a4157cff.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
