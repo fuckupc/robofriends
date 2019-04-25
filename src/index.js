85fd92cbd8428bd525b0ec3c02bf6b7a6295a660
upcliucong <upcliucong@gmail.com>
2019-04-25 22:48:33 +0800

    Before Finish, now can search

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { robots } from './robots';
-/*without exported default, props must surround {} */
 
-ReactDOM.render(
-    /* pass the props to CardList.js */
-    <CardList robots={robots} />
-    , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
