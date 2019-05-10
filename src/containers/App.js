9e7eb249447c6730294362596549d85233f8b2ff
upcliucong <upcliucong@gmail.com>
2019-05-10 18:36:52 +0800

    add redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,33 +1,57 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
+
+import { setSearchField } from '../actions';
+
+/*send state*/
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+/*send action */
+const mapDispatchToProps = (dispatch) => {
+    return {
+        /*input box that use type *//*setSearchField listens to the action, which receives a text */
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
 
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: [],
-            searchfield: ''
+            robots: []
+            /*,searchfield:''*/
+            /*searchfield is removed because of connect */
         }
     }
 
     componentDidMount() {
+        // console.log(this.props.store.getState());
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => this.setState({ robots: users }));
     }
 
+    /*onSearchChange is removed because of connect */
+    /* 
     onSearchChange = (e) => {
         this.setState({ searchfield: e.target.value })
-    }
+    } 
+    */
 
     render() {
-        const { robots, searchfield } = this.state;
+        const { robots } = this.state;  /*searchfield is removed because of connect */
+        const { searchField, onSearchChange } = this.props;
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
         /*in case the fetch fails */
         return (!robots.length) ?
@@ -35,7 +59,8 @@ class App extends Component {
         (
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} /> 
+                <SearchBox searchChange={onSearchChange} />
+                {/*this.onSearchChange, this is removed because it is from props, not state */}
                 <Scroll>
                     <CardList robots={filteredRobots} />
                 </Scroll>
@@ -44,4 +69,5 @@ class App extends Component {
     }
 }
 
-export default App;
+/*"connect" listens to the first part(state), and the second part(action), then send them to App*/
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/*connect redux */
+import { Provider } from 'react-redux';
+/*create store in redux */
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+/*import searchRobots from reducers.js */
+import { searchRobots } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*Using reducer to create store, inside which we can then use getState() */
+const store = createStore(searchRobots);
 
+ReactDOM.render(
+               /* Provider can pass down the store to every element inside App*/
+                <Provider store={store}>
+                   <App /> 
+                </Provider>, 
+                document.getElementById('root'));
 serviceWorker.unregister();
